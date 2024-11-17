26f2d0a5703be0de3aec317a9da57eedb2a2ab5c
Wiktoria Grymuza <wiktoria.grymuza@clurgo.com>
2024-11-17 21:49:23 +0100

    add main function

--- db-programming/Program.cs
+++ db-programming/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace db_programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
