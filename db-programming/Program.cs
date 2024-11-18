c4b553662309e67eeef4b29bc40404fdbc562f94
Wiktoria Grymuza <wiktoria.grymuza@clurgo.com>
2024-11-18 12:15:26 +0100

    lab 3 - delete comments

--- db-programming/Program.cs
+++ db-programming/Program.cs
@@ -6,19 +6,14 @@ namespace db_programming
     {
         static void Main(string[] args)
         {
-            // Create an instance of Employee class
             Employee employee = new Employee();
 
-            // Set values
             employee.FirstName = "John";
             employee.LastName = "Doe";
             employee.Position = "Developer";
 
-            // Assigning salary
-            employee.Salary = 4000m; // Valid salary
-            // employee.Salary = 3000m; // Invalid salary, below minimum salary
+            employee.Salary = 4000; 
 
-            // Display full employee data
             Console.WriteLine(employee.GetFullData());
         }
     }
