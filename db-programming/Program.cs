20f1b2ee72a8ec2b4b3f22de65c84955281a96de
Wiktoria Grymuza <w.grymuza@doittechnology.pl>
2024-12-15 21:51:28 +0100

    lab 8

--- db-programming/Program.cs
+++ db-programming/Program.cs
@@ -8,98 +8,22 @@ namespace db_programming
         {
             using (var context = new StudentContext())
             {
-                // Upewniamy się, że baza danych jest tworzona
                 context.Database.EnsureCreated();
 
-                // Exercise 1: Filter students
-                var filteredStudents = FilterStudentsByMajorAndStartYear(context, "Computer Science", 2020);
-                PrintStudents(filteredStudents);
+                List<Student> filteredStudents = StudentService.FilterStudentsByMajorAndStartYear(context, "Computer Science", 2020);
+                StudentService.PrintStudents(filteredStudents);
 
-                // Exercise 2: Projection (Select)
-                var selectedNames = GetStudentNames(context);
-                PrintNames(selectedNames);
+                List<Student> selectedNames = StudentService.GetStudentNames(context);
+                StudentService.PrintNames(selectedNames);
 
-                // Exercise 3: Sorting results
-                var sortedStudents = SortStudentsByLastNameAndFirstName(context);
-                PrintStudents(sortedStudents);
+                List<Student> sortedStudents = StudentService.SortStudentsByLastNameAndFirstName(context);
+                StudentService.PrintStudents(sortedStudents);
 
-                // Exercise 4: Grouping data (GroupBy)
-                var groupedByMajor = GroupStudentsByMajor(context);
-                PrintGroupedByMajor(groupedByMajor);
+                var groupedByMajor = StudentService.GroupStudentsByMajor(context);
+                StudentService.PrintGroupedByMajor(groupedByMajor);
 
-                // Exercise 5: Query syntax
-                var querySortedStudents = QuerySortedStudents(context, "Computer Science");
-                PrintStudents(querySortedStudents);
-            }
-        }
-
-        // Exercise 1: Filter students by major and start year
-        static List<Student> FilterStudentsByMajorAndStartYear(StudentContext context, string major,
-            int startYear)
-        {
-            return context.Students
-                .Where(s => s.Major == major && s.StartYear == startYear)
-                .ToList();
-        }
-
-        // Exercise 2: Select only First Name and Last Name
-        static List<Student> GetStudentNames(StudentContext context)
-        {
-            return context.Students
-                .Select(s => new Student { FirstName = s.FirstName, LastName = s.LastName })
-                .ToList();
-        }
-
-        // Exercise 3: Sort students by LastName and FirstName
-        static List<Student> SortStudentsByLastNameAndFirstName(StudentContext context)
-        {
-            return context.Students
-                .OrderBy(s => s.LastName)
-                .ThenBy(s => s.FirstName)
-                .ToList();
-        }
-
-        // Exercise 4: Group students by major
-        static List<IGrouping<string, Student>> GroupStudentsByMajor(StudentContext context)
-        {
-            return context.Students
-                .GroupBy(s => s.Major)
-                .ToList();
-        }
-
-        // Exercise 5: Query syntax for sorting students by LastName and FirstName
-        static List<Student> QuerySortedStudents(StudentContext context, string major)
-        {
-            return (from s in context.Students
-                where s.Major == major
-                orderby s.LastName, s.FirstName
-                select s).ToList();
-        }
-
-        // Print all students
-        static void PrintStudents(List<Student> students)
-        {
-            foreach (var student in students)
-            {
-                Console.WriteLine($"{student.FirstName} {student.LastName}");
-            }
-        }
-
-        // Print names only
-        static void PrintNames(List<Student> students)
-        {
-            foreach (var student in students)
-            {
-                Console.WriteLine($"{student.FirstName} {student.LastName}");
-            }
-        }
-
-        // Print grouped students by major
-        static void PrintGroupedByMajor(List<IGrouping<string, Student>> groupedByMajor)
-        {
-            foreach (var group in groupedByMajor)
-            {
-                Console.WriteLine($"{group.Key}: {group.Count()} students");
+                List<Student> querySortedStudents = StudentService.QuerySortedStudents(context, "Computer Science");
+                StudentService.PrintStudents(querySortedStudents);
             }
         }
     }
---
+++ db-programming/student/StudentService.cs
@@ -0,0 +1,74 @@
+﻿namespace db_programming.student;
+
+public static class StudentService
+{
+     // Exercise 1: Filter students by major and start year
+        public static List<Student> FilterStudentsByMajorAndStartYear(StudentContext context, string major,
+            int startYear)
+        {
+            return context.Students
+                .Where(s => s.Major == major && s.StartYear == startYear)
+                .ToList();
+        }
+
+        // Exercise 2: Select only First Name and Last Name
+        public static List<Student> GetStudentNames(StudentContext context)
+        {
+            return context.Students
+                .Select(s => new Student { FirstName = s.FirstName, LastName = s.LastName })
+                .ToList();
+        }
+
+        // Exercise 3: Sort students by LastName and FirstName
+        public static List<Student> SortStudentsByLastNameAndFirstName(StudentContext context)
+        {
+            return context.Students
+                .OrderBy(s => s.LastName)
+                .ThenBy(s => s.FirstName)
+                .ToList();
+        }
+
+        // Exercise 4: Group students by major
+        public static List<IGrouping<string, Student>> GroupStudentsByMajor(StudentContext context)
+        {
+            return context.Students
+                .GroupBy(s => s.Major)
+                .ToList();
+        }
+
+        // Exercise 5: Query syntax for sorting students by LastName and FirstName
+        public static List<Student> QuerySortedStudents(StudentContext context, string major)
+        {
+            return (from s in context.Students
+                where s.Major == major
+                orderby s.LastName, s.FirstName
+                select s).ToList();
+        }
+
+        // Print all students
+        public static void PrintStudents(List<Student> students)
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}");
+            }
+        }
+
+        // Print names only
+        public static void PrintNames(List<Student> students)
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName}");
+            }
+        }
+
+        // Print grouped students by major
+        public static void PrintGroupedByMajor(List<IGrouping<string, Student>> groupedByMajor)
+        {
+            foreach (var group in groupedByMajor)
+            {
+                Console.WriteLine($"{group.Key}: {group.Count()} students");
+            }
+        }
+}
